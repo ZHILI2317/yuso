f4810cf8b71a114bf2f297c67f2bf23220e92ccb
卢兴达 <luxingda3@163.com>
2025-11-28 15:43:14 +0800

    项目初始化

---
+++ yuso-frontend/src/components/PictureList.vue
@@ -0,0 +1,12 @@
+<template>
+  <img :src="logo" class="logo" />
+</template>
+
+<script setup lang="ts">
+import logo from "../assets/logo.png";
+</script>
+<style scoped>
+.logo {
+  width: 200px;
+}
+</style>
